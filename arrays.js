991fe24521d7263bcf637efd79717e53cee9b81e
hmelo1 <heriberto@melo.nyc>
2017-10-04 02:28:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,7 @@ function destructivelyAddElementToBeginningOfArray(someArray, someElement){
   someArray.unshift(someElement)
   return someArray
 }
+function addElementToEndOfArray(someArray, someElement){
+  someArray2 = [...someArray, someElement]
+  return someArray2
+}
