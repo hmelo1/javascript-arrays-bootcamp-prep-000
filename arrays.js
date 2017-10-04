fe90c263f18b75ddc0133986b21360f54fc4222e
hmelo1 <heriberto@melo.nyc>
2017-10-04 01:51:04 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(array1, element1){
+  array1.unshift(element1)
+  return array1
+}
+function destructivelyAddElementToBeginningOfArray(array1, element1){
+
+}
